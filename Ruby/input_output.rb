ef909774a25ff77bc63b6583b9f07cd46f8c7943
jamalsimmonds <jsimmonds@jellyvision.com>
2024-04-17 18:45:44 -0500

    add input

--- Ruby/input_output.rb
+++ Ruby/input_output.rb
@@ -60,4 +60,19 @@ Ruby Is Cool
 irb(main):008:0> pp [1,2]
 [1, 2]
 => [1, 2]
+
 ## Input
+
+#gets
+# use .chomp to remove new line character
+irb(main):001:0> gets
+The Odin Project
+=> "The Odin Project\n"
+
+irb(main):001:0> new_string = gets.chomp
+This is a sentence.
+=> "This is a sentence."
+
+irb(main):002:0> puts new_string
+This is a sentence.
+=> nil
