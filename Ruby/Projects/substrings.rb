05c3567e15347bb383224e8b718bccc19b942643
jamalsimmonds <jsimmonds@jellyvision.com>
2024-06-26 14:46:20 -0500

    substring project

---
+++ Ruby/Projects/substrings.rb
@@ -0,0 +1,29 @@
+=begin
+  Implement a method #substrings that takes a word as the first argument and then an array of valid substrings
+   (your dictionary) as the second argument. 
+  It should return a hash listing each substring (case insensitive)
+   that was found in the original string and how many times it was found.
+=end
+
+def substrings (wordInput, dict)
+  stor = Hash.new(0)
+  wordInput.downcase!
+  arrOfInput = wordInput.split(" ")
+  # puts arrOfInput
+  dict.each do |sub|
+    arrOfInput.each do |word|
+      if word.include?(sub)
+        stor[sub] += 1
+      end
+    end
+  end
+  puts stor
+end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+substrings("low down below", dictionary)
+substrings("Howdy partner, sit down! How's it going?", dictionary)
+
+#  > substrings("Howdy partner, sit down! How's it going?", dictionary)
+#=> { "down" => 1, "go" => 1, "going" => 1, "how" => 2, "howdy" => 1, "it" => 2, "i" => 3, "own" => 1, "part" => 1, "partner" => 1, "sit" => 1 }
