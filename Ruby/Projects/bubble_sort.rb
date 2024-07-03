8d937f394c736cef1f94bafa0c385dcd396a8077
simmonds02 <jsimmonds2120@gmail.com>
2024-07-03 17:37:30 -0500

    bubble sort

--- Ruby/Projects/bubble_sort.rb
+++ Ruby/Projects/bubble_sort.rb
@@ -1,21 +1,21 @@
   
   
-  def bubble_sort(array = Array.new)
-    # take array and start sort
-    (array.length - 1).times do
-      sortIndex = 0
-      while sortIndex < (array.length - 1)
-      # check if current is less than element to the right of index
-        if array[sortIndex] > array[sortIndex+1]
-          temp = array[sortIndex]
-          array[sortIndex] = array[sortIndex+1]
-          array[sortIndex+1] = temp
-        end
-        sortIndex+=1
+def bubble_sort(array = Array.new)
+  # take array and start sort
+  (array.length - 1).times do
+    sortIndex = 0
+    while sortIndex < (array.length - 1)
+    # check if current is less than element to the right of index
+      if array[sortIndex] > array[sortIndex+1]
+        temp = array[sortIndex]
+        array[sortIndex] = array[sortIndex+1]
+        array[sortIndex+1] = temp
       end
+      sortIndex+=1
     end
-    p array
   end
+  p array
+end
 
 
 
