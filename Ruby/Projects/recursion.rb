24bbfd6ca94359ce07029dfe07719dc1a02c9614
simmonds02 <jsimmonds2120@gmail.com>
2025-05-08 16:10:31 -0500

    setup project file

---
+++ Ruby/Projects/recursion.rb
@@ -0,0 +1,4 @@
+# We will explain an example of how an enumerable works to give you an idea of how to start tackling these methods. 
+# Specifically we’re going to break down the #find enumerable method. 
+# The #find method finds the first element that matches the given block’s condition and returns it. 
+# If one isn’t found, it returns nil.
