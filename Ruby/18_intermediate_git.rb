db81428ec43b6edc0dbaec56c00e842c3a6f6f56
simmonds02 <jsimmonds2120@gmail.com>
2025-11-13 10:46:23 -0600

    skeleton

---
+++ Ruby/18_intermediate_git.rb
@@ -0,0 +1,9 @@
+# Lesson overview
+# This section contains a general overview of topics that you will learn in this lesson.
+
+# History-changing Git commands.
+# Different ways of changing history.
+# Using remotes to change history.
+# Dangers of history-changing operations.
+# Best practices of history-changing operations.
+# Pointers.
